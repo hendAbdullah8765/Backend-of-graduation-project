b975cb63976bcecd58679ba60aa3fa7e8d39fdf9
hendAbdullah8765 <h96396588@gmail.com>
2025-05-04 14:03:51 +0300

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -55,6 +55,7 @@ location: {
 }, 
 {timestamps: true}
 );
+UserSchema.index({ address: "2dsphere" });
 const setImageURL = (doc)=>{
   if(doc.profileImg){
     const imageUrl =`${process.env.BASE_URL}/upload/users/${doc.profileImg}`
