8e34cc0258c213aa72d224e69d3a1af45f04da48
hendAbdullah8765 <h96396588@gmail.com>
2025-05-05 21:35:02 +0300

    Update OrphanageModel.js

--- models/OrphanageModel.js
+++ models/OrphanageModel.js
@@ -5,50 +5,82 @@ const orphanageSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  admin:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+  adminName: {
+    type: String,
     required: true
   },
- 
   image: {
-    type: String 
+    type: String,
+    required: false,
   },
+  address: {
+    type: String,
+    required: true,
+  },
+
   slug: {
     type: String
   },
-
-  phone: String,
-  currentChildren:{
-    type: Number
+ 
+  currentChildren: {
+    type: Number,
+    required: [true, 'Number of children is required'],
+    min: 0
+  },
+  totalCapacity: {
+    type: Number,
+    required: [true, 'Total capacity is required'],
+    min: 1
   },
   active:{
     type : Boolean,
     default: true
   },
-address: {
-  type: String,
-  required: true,
-},
+  staffCount: {
+    type: Number,
+    required: [true, 'Staff count is required'],
+    min: 0
+  },
+  establishedDate: {
+    type: Date,
+    required: [true, 'Established date is required']
+  },
+  phone: String,
+  birthdate: {
+    type: Date,
+    required: [true, 'Birthdate is required']
+  },
+
+  workSchedule: {
+    workDays: {
+      type: [String], 
+      enum: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      default: []
+    },
+  workHours: {
+      type: [String], 
+      enum: ['Morning 6am-12pm', 'Afternoon 12pm-4pm', 'Evening 4pm-8pm', 'Night 8pm-12am'],
+      default: []
+    },
+
+  }
 }, 
 {timestamps: true}
 );
 
-const setImageURL = (doc)=>{
-  if(doc.image){
-    const imageUrl =`${process.env.BASE_URL}/upload/users/${doc.image}`
+const setImageURL = (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/users/${doc.image}`;
     doc.image = imageUrl;
   }
-
-}
+};
 // getAll / update / getOne
-orphanageSchema.post('init', (doc) => {
-  setImageURL(doc)
+orphanageSchema.post("init", (doc) => {
+  setImageURL(doc);
 });
-
 //create
-orphanageSchema.post('save', (doc) => {
-  setImageURL(doc)
+orphanageSchema.post("save", (doc) => {
+  setImageURL(doc);
 });
 
 module.exports = mongoose.model('Orphanage', orphanageSchema);
