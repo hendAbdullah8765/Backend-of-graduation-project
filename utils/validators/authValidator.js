6764f7fc78ce137646d4ddfd78d9a6b00fb3ca80
hendAbdullah8765 <h96396588@gmail.com>
2025-05-05 21:39:09 +0300

    Update authValidator.js

--- utils/validators/authValidator.js
+++ utils/validators/authValidator.js
@@ -1,10 +1,12 @@
 const slugify = require('slugify');
-const { check  } = require('express-validator');
+const { check , body } = require('express-validator');
 const validatorMiddleware = require('../../middlewares/validatorMiddleware');
 const Orphanage = require('../../models/OrphanageModel')
 const User = require('../../models/UserModel')
 
 exports.signupValidator = [
+check('role').optional().isIn(['Orphanage', 'Donor']).withMessage('Invalid role'), 
+
   check('name')
     .notEmpty().withMessage('User name is required')
     .isLength({ min: 2 }).withMessage('User name is too short')
@@ -43,14 +45,49 @@ exports.signupValidator = [
   
   check('image').optional(), 
 
-  check('phone').optional().isMobilePhone(['ar-EG','ar-SA'])
+  check('phone').isMobilePhone(['ar-EG','ar-SA'])
   .withMessage('invalid phone numper only accepted Egy and SA phone numbers'),
  
-  check('role').optional().isIn(['Orphanage', 'Donor']).withMessage('Invalid role'), 
-  
   check('address')
     .notEmpty().withMessage('User address is required'),
-  validatorMiddleware
+    
+  
+    check('adminName').if(body('role').equals('Orphanage'))
+      .not().isEmpty().withMessage('Name is required for Orphanage role'),
+  
+    check('currentChildren').if(body('role').equals('Orphanage'))
+      .isInt({ min: 0 }).withMessage('CurrentChildren must be a non-negative number'),
+  
+    check('totalCapacity').if(body('role').equals('Orphanage'))
+      .isInt({ min: 1 }).withMessage('TotalCapacity must be at least 1'),
+  
+    check('staffCount').if(body('role').equals('Orphanage'))
+      .isInt({ min: 0 }).withMessage('StaffCount must be a non-negative number'),
+  
+    check('establishedDate').if(body('role').equals('Orphanage'))
+      .notEmpty().withMessage('EstablishedDate is required')
+      .isISO8601().withMessage('Invalid date format'),
+      check('birthdate').isDate().notEmpty()
+      .withMessage('Birthdate is required'),
+    check('workSchedule.workDays').optional().isArray().withMessage('workDays must be an array')
+      .custom((val) => {
+        console.log(val);  
+        const validDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+        const invalid = val.some(day => !validDays.includes(day));
+        if (invalid) throw new Error('Invalid work day');
+        return true;
+      }),
+  
+    check('workSchedule.workHours').optional().isArray().withMessage('workHours must be an array')
+      .custom((val) => {
+        console.log(val);  
+        const validHours = ['Morning 6am-12pm', 'Afternoon 12pm-4pm', 'Evening 4pm-8pm', 'Night 8pm-12am'];
+        const invalid = val.some(hr => !validHours.includes(hr));
+        if (invalid) throw new Error('Invalid work hour');
+        return true;
+      }),
+
+    validatorMiddleware
 ];
 exports.loginValidator = [
   check('email')
