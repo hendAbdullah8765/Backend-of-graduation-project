0de88ec1409e805516d19130f2fa72c2706958ca
hendAbdullah8765 <h96396588@gmail.com>
2025-05-08 21:48:07 +0300

    Update PostService.js

--- services/PostService.js
+++ services/PostService.js
@@ -1,6 +1,7 @@
 const sharp = require ('sharp');
 const {v4: uuidv4} = require('uuid');
 const asyncHandler = require ('express-async-handler')
+const ApiError = require ('../utils/ApiError')
 const factory = require('./handlerFactory');
 const Post = require("../models/PostModel");
 const {uploadMixOfImages} = require ('../middlewares/uploadImagesMiddleware')
@@ -69,24 +70,44 @@ exports.allowedToModifyPost = asyncHandler(async (req, res, next) => {
 // @desc  get list of Post
 // @route Get /api/v1/posts
 // @access Public
-exports.getPosts = factory.getAll(Post, 'Post', {
-  path: 'user',
-  select: 'name'
-})
+exports.getPosts = factory.getAll(Post, 'Post', [
+  {
+    path: 'user',
+    select: 'name'
+  },
+  {
+    path: 'repostedFrom',
+    select: 'content user image repostCount',
+    populate: {
+      path: 'user',
+      select: 'name'
+    }
+  }
+])
 
 // @desc  get spacific Post by id
 // @route Get /api/v1/posts/:id  
 // @access Public
-exports.getPost = factory.getOne(Post, {
-  path: 'user',
-  select: 'name'
-});  
+exports.getPost = factory.getOne(Post, [
+  {
+    path: 'user',
+    select: 'name'
+  },
+  {
+    path: 'repostedFrom',
+    select: 'content user image repostCount',
+    populate: {
+      path: 'user',
+      select: 'name'
+    }
+  }
+])
 // @desc  create Post
 // @route Post /api/v1/posts 
 // @access Private
 exports.createPost = factory.createOne(Post, {
   path: 'user',
-  select: 'name'
+  select: 'name email'
 });
 
 
@@ -96,7 +117,7 @@ exports.createPost = factory.createOne(Post, {
 
 exports.updatePost = factory.updateOne(Post, {
   path: 'user',
-  select: 'name'
+  select: 'name email'
 });
 
 
@@ -104,3 +125,41 @@ exports.updatePost = factory.updateOne(Post, {
 // @route delete /api/v1/posts/:id
 // @access Public
 exports.deletePost = factory.deleteOne(Post);
+
+// @desc  create a repost
+// @route Post /api/v1/posts/:id/repost
+// @access Private
+exports.createRepost = asyncHandler(async (req, res, next) => {
+  const originalPost = await Post.findById(req.params.id);
+  if (!originalPost) {
+    return next(new ApiError('Original post not found', 404));  
+  }
+
+  const repost = await Post.create({
+    content: originalPost.content,  
+    user: req.user._id,            
+    image: originalPost.image,      
+    images: originalPost.images,    
+    repostedFrom: originalPost._id, 
+    slug: originalPost.slug + '-repost'   
+  });
+  originalPost.repostCount += 1;
+  await originalPost.save();
+  await repost.populate([
+    {
+      path: 'repostedFrom',
+      select: 'content user image',
+      populate: {
+        path: 'user',
+        select: 'name email'
+      }
+    },
+    {
+      path: 'user',
+      select: 'name email'
+    }
+  ]);
+  
+  res.status(201).json({  data: repost });  
+});
+
