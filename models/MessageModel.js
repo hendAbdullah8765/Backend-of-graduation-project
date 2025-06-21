e8a6772ce97c1b79189b308788ec996d2942686f
Hend Abdullah <h96396588@gmail.com>
2025-06-21 19:58:34 +0300

    Update MessageModel.js

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -2,11 +2,40 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const messageSchema = new Schema({
-  senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  message: { type: String, trim: true, required: true },
-  createdAt: { type: Date, default: Date.now },
+const messageSchema = new Schema(
+  {
+    chatId: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
+
+    senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+    receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+    message: { type: String, trim: true },
+
+    image: { type: String }, // اختياري لو عايزة تبعتي صورة
+
+    isSeen: { type: Boolean, default: false },
+
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+const setImageURL = (doc)=>{
+  if(doc.image){
+    const imageUrl =`${process.env.BASE_URL}/upload/message/${doc.image}`
+    doc.image = imageUrl;
+  }
+}
+// getAll / update / getOne
+messageSchema.post('init', (doc) => {
+  setImageURL(doc)
+});
+
+//create
+messageSchema.post('save', (doc) => {
+  setImageURL(doc)
 });
 
 module.exports = mongoose.model("Message", messageSchema);
